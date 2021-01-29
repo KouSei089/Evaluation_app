0f6a75ef7bcbbfdedbc87c56231f1a390c76e9c2
KouSei089 <qq00891829@icloud.com>
2021-01-29 18:45:52 +0900

    second

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,12 +1,35 @@
 <template>
   <v-app id="app">
-    <v-btn>Vuetifyのボタン</v-btn>
-    <div>
-      <p>{{ message }}</p>
-    </div>
+    <v-card v-for="evaluation in evaluations" :key="evaluation.id">
+      <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>{{ evaluation.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    </v-card> 
+        <!--{{ evaluation.services }} : {{ evaluation.language }}： {{ evaluation.description }} -->
   </v-app> 
 </template>
 
 <script>
+import axios from 'axios';
 
+export default {
+  data: function() {
+    return {
+      evaluations: "evaluations"
+    }
+  },
+  mounted() {
+    this.setEvaluation();
+  },
+  methods: {
+    setEvaluation: function () {
+      axios.get('/api/evaluations')
+      .then(response => (
+        this.evaluations = response.data
+      ))
+    }
+  }
+}
 </script>
